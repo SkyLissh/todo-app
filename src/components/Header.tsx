d3e0121a46fdc57f33f8082e11038e381663e331
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-03-01 01:39:21 -0600

    :lipstick: Header Component
    Image and title app

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import "src/components/Header.css";
+
+export default function Header() {
+	return (
+		<header className="header">
+			<h1 className="header__title">TODO</h1>
+			<span className="material-icons icon icon--header">nightlight_round</span>
+		</header>
+	);
+}
