f2567ae75f0851a039c7b99bffe0de627820cea1
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-04-18 22:59:04 -0500

    :sparkles: Add Task
    Logic for add tasks to list

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,46 @@
+import { Dispatch, FormEvent, SetStateAction, useRef } from "react";
 import { Card, CardItem } from "src/components";
 
 import "src/components/Input.css";
 
-export function Input() {
+import { Task } from "src/models/task";
+import { LocalCRUD } from "src/models/local-crud";
+
+type Props = {
+	setTasks: Dispatch<SetStateAction<Task[]>>;
+};
+
+export function Input({ setTasks }: Props) {
+	const inputTask = useRef<HTMLInputElement>(null);
+
+	function onSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+
+		if (inputTask.current) {
+			const task: Task = {
+				id: Date.now(),
+				description: inputTask.current.value,
+				completed: false
+			};
+
+			setTasks((prevTasks) => [...prevTasks, task]);
+			LocalCRUD.add(task);
+
+			inputTask.current.value = "";
+		}
+	}
+
 	return (
 		<Card>
-			<CardItem circle>
-				<input className="card__input" type="text" placeholder="Create new todo..." />
+			<CardItem>
+				<form className="card__form" onSubmit={onSubmit}>
+					<input
+						ref={inputTask}
+						className="card__input"
+						type="text"
+						placeholder="Create new todo..."
+					/>
+				</form>
 			</CardItem>
 		</Card>
 	);
---
+++ src/models/task.ts
@@ -0,0 +1,11 @@
+export interface OptionalTask {
+	id?: number;
+	description?: string;
+	completed?: boolean;
+}
+
+export interface Task {
+	id: number;
+	description: string;
+	completed: boolean;
+}
